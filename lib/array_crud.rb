a8c2f9b7e7ac4a213b5537d86a806698cf55ab4c
Austin Hernandez <ahernandez1170@gmail.com>
2021-01-13 22:39:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,4 +1,5 @@
 def create_an_empty_array
+<<<<<<< HEAD
   []
 end
 
@@ -8,18 +9,43 @@ end
 
 def add_element_to_end_of_array(array, element)
   array.push(element)
+=======
+  array = []
+end
+
+def create_an_array
+  array = ["one", "two", "three", "four"]
+  puts array
+end
+
+def add_element_to_end_of_array(array, element)
+  array << (element)
+  puts array
+>>>>>>> 6acee99740386f397ab2f22daae15eb0fbb66d40
 end
 
 def add_element_to_start_of_array(array, element)
   array.unshift(element)
+<<<<<<< HEAD
+=======
+  puts array
+>>>>>>> 6acee99740386f397ab2f22daae15eb0fbb66d40
 end
 
 def remove_element_from_end_of_array(array)
   array.pop
+<<<<<<< HEAD
+=======
+  puts array
+>>>>>>> 6acee99740386f397ab2f22daae15eb0fbb66d40
 end
 
 def remove_element_from_start_of_array(array)
   array.shift
+<<<<<<< HEAD
+=======
+  puts array
+>>>>>>> 6acee99740386f397ab2f22daae15eb0fbb66d40
 end
 
 def retrieve_element_from_index(array, index_number)
